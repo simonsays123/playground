fce5507d599312253c0a07ebf41f1f38765dc776
simon <smnluo@gmail.com>
2016-03-09 23:43:46 -0800

    check in express

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/about', function (req, res) {
+  res.send('about');
+});
+
+
+app.get('/random.text', function (req, res) {
+  res.send('random.text');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
